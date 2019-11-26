47dae17be24877a193334608eef7d976a9bb29f5
eitrunix <52928831+eitrunix@users.noreply.github.com>
2019-11-26 15:58:21 -0500

    Added Settings Menu, I cant get the game to chnage Resolutions

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    Resolution[] resolutions;
+    public Dropdown resolutuonDropdown;
+
+    public AudioMixer audioMixer;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutuonDropdown.ClearOptions();
+        int currentResolutionIndex = 0;
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutuonDropdown.AddOptions(options);
+        resolutuonDropdown.value = currentResolutionIndex;
+        resolutuonDropdown.RefreshShownValue();
+    }
+    // to Set our volume
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+        Debug.Log("Main Volume Level: " + volume);
+    }
+    public void ToggleMasterVolume(bool IsToggled)
+    {
+        audioMixer.SetFloat("MasterVolume", -80);
+    }
+
+    public void SetSoundVolume(float Soundvolume)
+    {
+        audioMixer.SetFloat("SoundsVolume", Soundvolume);
+        Debug.Log("Sounds Volume Level: " + Soundvolume);
+    }
+    public void SetMusicVolume(float Musicvolume)
+    {
+        audioMixer.SetFloat("MusicVolume", Musicvolume);
+
+        Debug.Log("Music Volume Level: " + Musicvolume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
