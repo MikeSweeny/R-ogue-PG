615cf60c1efc98e73b19286d961c5c30f36875fa
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-11-26 16:36:50 -0500

    Audio Manager Scripts Fixed
    SFX and BKG Audio scripts fixed to have functions play whatever song or sfx file needed to be called and stops the previous current song playing. sfx only play once so no need to stop them when starting another.

---
+++ Assets/Scripts/Game/Audio/AudioManagerBKGMusic.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerBKGMusic : MonoBehaviour
+{
+    //Current Song Playing
+    private AudioSource currentSong;
+
+    //List of background audio
+    public AudioClip startScreen;
+    public AudioClip tutorialState;
+    public AudioClip winScreen;
+    public AudioClip deathScreen;
+    public AudioClip battleState;
+    public AudioClip ritualRoomState;
+
+    //Instance of Audio Manager
+    public static AudioManagerBKGMusic instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayStartScreenSong()
+    {
+        currentSong.Stop();
+        currentSong.clip = startScreen;
+        currentSong.Play();
+    }
+    public void PlayDeathScreenSong()
+    {
+        currentSong.Stop();
+        currentSong.clip = deathScreen;
+        currentSong.Play();
+    }
+    public void PlayWinScreenSong()
+    {
+        currentSong.Stop();
+        currentSong.clip = winScreen;
+        currentSong.Play();
+    }
+    public void PlayTutorialStateSong()
+    {
+        currentSong.Stop();
+        currentSong.clip = tutorialState;
+        currentSong.Play();
+    }
+    public void PlayBattleStateSong()
+    {
+        currentSong.Stop();
+        currentSong.clip = battleState;
+        currentSong.Play();
+    }
+    public void PlayRitualRoomStateSong()
+    {
+        currentSong.Stop();
+        currentSong.clip = ritualRoomState;
+        currentSong.Play();
+    }
+    public void StopSong()
+    {
+        currentSong.Stop();
+    }
+}
---
+++ Assets/Scripts/Game/Audio/AudioManagerSFX.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManagerSFX : MonoBehaviour
+{    
+    //Current SFX Playing
+    private AudioSource currentSFX;
+
+    //List of SFX audio
+    public AudioClip playerHitSFX;
+    public AudioClip playerDeathSFX;
+    public AudioClip playerJumpSFX;
+    public AudioClip enemyHitSFX1;
+    public AudioClip enemyHitSFX2;
+    public AudioClip enemyHitSFX3;
+    public AudioClip enemyDeathSFX1;
+    public AudioClip enemyDeathSFX2;
+    public AudioClip enemyDeathSFX3;
+    public AudioClip spellCast1;
+    public AudioClip spellCast2;
+    public AudioClip spellCast3;
+    public AudioClip enemySpellCast1;
+    public AudioClip enemySpellCast2;
+    public AudioClip enemySpellCast3;
+    public AudioClip torchAudio;
+    public AudioClip perkDaisAudio;
+    public AudioClip leverSFX;
+    public AudioClip perkPickUpSFX;
+    public AudioClip boneDropped;
+    public AudioClip boneCollected;
+    public AudioClip buttonPress;
+    public AudioClip pauseEnter;
+    public AudioClip pauseExit;
+
+    //Instance of Audio Manager
+    public static AudioManagerSFX instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    public void PlayerHitSFX()
+    {
+        currentSFX.clip = playerHitSFX;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void PlayerDeathSFX()
+    {
+        currentSFX.clip = playerDeathSFX;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void PlayerJumpSFX()
+    {
+        currentSFX.clip = playerJumpSFX;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void SpellCastSFX1()
+    {
+        currentSFX.clip = spellCast1;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void SpellCastSFX2()
+    {
+        currentSFX.clip = spellCast2;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void SpellCastSFX3()
+    {
+        currentSFX.clip = spellCast3;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemyHitSFX1()
+    {
+        currentSFX.clip = enemyHitSFX1;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemyHitSFX2()
+    {
+        currentSFX.clip = enemyHitSFX2;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemyHitSFX3()
+    {
+        currentSFX.clip = enemyHitSFX3;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemyDeathSFX1()
+    {
+        currentSFX.clip = enemyDeathSFX1;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemyDeathSFX2()
+    {
+        currentSFX.clip = enemyDeathSFX2;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemyDeathSFX3()
+    {
+        currentSFX.clip = enemyDeathSFX3;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemySpellCastSFX1()
+    {
+        currentSFX.clip = enemySpellCast1;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemySpellCastSFX2()
+    {
+        currentSFX.clip = enemySpellCast2;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void EnemySpellCastSFX3()
+    {
+        currentSFX.clip = enemySpellCast3;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void TorchSFX()
+    {
+        currentSFX.clip = torchAudio;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void PerkDaisSFX()
+    {
+        currentSFX.clip = perkDaisAudio;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void LeverSFX()
+    {
+        currentSFX.clip = leverSFX;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void PerkPickUpSFX()
+    {
+        currentSFX.clip = perkPickUpSFX;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void BoneDroppedSFX()
+    {
+        currentSFX.clip = boneDropped;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void BoneCollectedSFX()
+    {
+        currentSFX.clip = boneCollected;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void ButtonPressedSFX()
+    {
+        currentSFX.clip = buttonPress;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void PauseEnterSFX()
+    {
+        currentSFX.clip = pauseEnter;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+    public void PauseExitSFX()
+    {
+        currentSFX.clip = pauseExit;
+        currentSFX.Play();
+        currentSFX.Stop();
+    }
+}
