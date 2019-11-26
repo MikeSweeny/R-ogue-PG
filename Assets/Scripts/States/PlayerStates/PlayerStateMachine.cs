5a28cae8f8dc3dac8636121ae481803f234041bf
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2019-11-26 15:57:18 -0500

    State machine work

---
+++ Assets/Scripts/States/PlayerStates/PlayerStateMachine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerState
+{
+    protected GameManager m_gameManager;
+    public PlayerState(GameManager gm)
+    {
+        m_gameManager = gm;
+    }
+
+    public abstract void StateEnter();
+    public abstract void StateExit();
+    public abstract void StateUpdate();
+}
