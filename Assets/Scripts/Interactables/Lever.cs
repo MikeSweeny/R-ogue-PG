3fc39bd1dcbcc1bb37921cb862d3aaa764f75603
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-11-27 16:47:01 -0500

    MLD/Torch Prefab/ Interactable and Lever Script
    -Master Level Design started with new background and terrain
    -Torch prefab created and placed in some areas(can be changed)
    -Interactable base script made and base lever created.

---
+++ Assets/Scripts/Interactables/Lever.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : Interactable
+{
+    public void LeverOn()
+    {
+        setIsOn(true);
+    }
+    public void LeverOff()
+    {
+        setIsOn(false);
+    }
+}
