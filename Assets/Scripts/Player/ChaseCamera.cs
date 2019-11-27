ff7c6ca0b510418b3700e22fe259979d36dd6711
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-11-27 14:40:50 -0500

    Revert "Implementing Chase Camera/ Crosshair Scripts"
    This reverts commit 793c0de158dadc2303dc0593bbf810cfb38aea9b.

--- Assets/Scripts/Player/ChaseCamera.cs
+++ Assets/Scripts/Player/ChaseCamera.cs
@@ -4,103 +4,80 @@ using UnityEngine;
 
 public class ChaseCamera : MonoBehaviour
 {
-    public GameObject target;
-    public float heightOffset = 1.7f;
-    public float distance = 12.0f;
+    public Transform car;
+    public float rotationDamping = 3f;
+    public float heightDamping = 2f;
+    private float desiredAngle = 0;
+    private float distance = 8;
+    private float desiredDistance;
+    private float height = 3.75f;
+    private bool cameraReset = false;
 
-    public float offsetFromWall = 0.1f;
-    public float maxDistance = 20f;
-    public float minDistance = 0.6f;
-    public float xSpeed = 200.0f;
-    public float ySpeed = 200.9f;
-    public float yMinLimit = -80f;
-    public float yMaxLimit = 80.0f;
-    public float autoRotSpeed = 3.0f;
-
-    public LayerMask collisionLayers = -1;
-    public bool alwaysRotToRearOfTarget = false;
-    public bool allowMouseInputX = true;
-    public bool allowMouseInputY = true;
-
-
-    float xDeg = 0.0f;
-    float yDeg = 0.0f;
-    float currentDistance;
-    float desiredDistance;
-    bool rotBehind = false;
-    bool mouseSideButton = false;
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        Vector3 angles = transform.eulerAngles;
-        xDeg = angles.x;
-        yDeg = angles.y;
-        currentDistance = distance;
         desiredDistance = distance;
-
-        rotBehind = alwaysRotToRearOfTarget;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    private void FixedUpdate()
     {
+        float currentAngle = transform.eulerAngles.y;
+        float currentHeight = transform.position.y;
 
-        RotateBehindTarget();
+        desiredAngle = car.eulerAngles.y;
 
-        yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
-
-        Quaternion rotation = Quaternion.Euler(yDeg, xDeg, 0);
-
-        desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
-
-
-        Vector3 vTargetOffset = new Vector3(0, -heightOffset, 0);
-        Vector3 position = target.transform.position - (rotation * Vector3.forward * desiredDistance + vTargetOffset);
-
-        Vector3 trueTargetPosition = new Vector3(target.transform.position.x, target.transform.position.y + heightOffset, target.transform.position.z);
-
-        currentDistance = Mathf.Clamp(currentDistance, minDistance, maxDistance);
-
-        position = target.transform.position - (rotation * Vector3.forward * currentDistance + vTargetOffset);
-
-        transform.rotation = rotation;
-        transform.position = position;
-
-    }
-
-    private void RotateBehindTarget()
-    {
-        float targetRotationAngle = target.transform.eulerAngles.y;
-        float currentRotationAngle = transform.eulerAngles.y;
-        xDeg = Mathf.LerpAngle(currentRotationAngle, targetRotationAngle, autoRotSpeed * Time.deltaTime);
-
-        if (targetRotationAngle == currentRotationAngle)
+        // if backing up
+        if (Input.GetKey(KeyCode.S))
         {
-            if (!alwaysRotToRearOfTarget)
+            Vector3 localVelocity = car.InverseTransformDirection(car.GetComponent<Rigidbody>().velocity);
+            if (localVelocity.z < -0.5f)
             {
-                rotBehind = false;
-
+                desiredAngle += 180;
             }
         }
-        else
+
+
+        float desiredHeight = car.position.y + height;
+
+        currentAngle = Mathf.LerpAngle(currentAngle, desiredAngle, rotationDamping * Time.deltaTime);
+        currentHeight = Mathf.Lerp(currentHeight, desiredHeight, heightDamping * Time.deltaTime);
+
+        Quaternion currentRotation = Quaternion.Euler(0, currentAngle, 0);
+
+        Vector3 finalPosition = car.position - (currentRotation * Vector3.forward * distance);
+        finalPosition.y = currentHeight;
+        transform.position = finalPosition;
+        transform.LookAt(car);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "ButtRocket")
         {
-            rotBehind = true;
+            distance -= 2f;
         }
     }
 
-    float ClampAngle(float angle, float min, float max)
+    private void OnTriggerExit(Collider other)
     {
-        if (angle < -360f)
-            angle += 360f;
+        if (other.gameObject.tag != "ButtRocket")
+        {
+            CameraSlowReset();
+        }
+    }
+    private void Update()
+    {
+        if (distance <= desiredDistance && cameraReset)
+        {
+            distance += 0.01f;
+            return;
+        }
+        cameraReset = false;
+    }
 
-        if (angle > 360f)
-            angle -= 360f;
-
-        return Mathf.Clamp(angle, min, max);
+    private void CameraSlowReset()
+    {
+        cameraReset = true;
     }
 }
-
 
 
