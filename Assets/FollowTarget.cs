360c2e2fb6e35cd4dba17fe4d71b790ea639aca5
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2019-11-27 13:34:39 -0500

    Baked the Walls

--- Assets/FollowTarget.cs
+++ Assets/FollowTarget.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class FollowTarget : MonoBehaviour
 {
+    private GameObject player;
     public Transform target;
 
     private NavMeshAgent agent;
@@ -12,12 +13,16 @@ public class FollowTarget : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        target = player.position;
         agent.SetDestination(target.position);
 
     }
