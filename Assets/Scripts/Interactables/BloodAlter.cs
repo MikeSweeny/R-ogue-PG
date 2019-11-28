4e80ecd67518e608f07b907df4d761601cbfad98
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-11-28 15:06:13 -0500

    Blood Alter Script
    - Added to the blood alter script for when the blood alter is idle it increases the ammount of blood in the alter over time up to the max amount allowed.
    -Commented out sweenys state change cause its not ready yet.

--- Assets/Scripts/Interactables/BloodAlter.cs
+++ Assets/Scripts/Interactables/BloodAlter.cs
@@ -4,33 +4,45 @@ using UnityEngine;
 
 public class BloodAlter : Interactable
 {
-    //private int maxBlood = 100;
-    //private int minBlood = 0;
-    //private int currentBlood = 100;
-    //public GameObject blood;
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if(Input.GetKeyDown(KeyCode.E))
-    //    {
-    //        PlayerInteract();
-    //    }
-    //}
+    private int maxZ = 3;
+    private int minZ = -2;
+    public Transform currentZ;
+    private int maxBlood = 100;
+    private int minBlood = 0;
+    private int currentBlood = 100;
+    public GameObject blood;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlayerInteract();
+        }
+       if(currentZ.transform.position.z < maxZ && currentZ.transform.position.z > minZ)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Vector3 temp = blood.transform.position;
+                temp.z += 1;
+                Vector3 newPosition = new Vector3(blood.transform.position.x, blood.transform.position.y, temp.z);
+            }
+        }
+    }
 
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    setIsOn(true);
-    //}
-    //public void PlayerInteract()
-    //{
-    //    if(isOn && playerHealth < maxHealth && currentBlood > minBlood && currentBlood <= maxBlood)
-    //    {
-    //        bones = bones - 1;
-    //        currentBlood = currentBlood - 1;
-    //        playerHealth = playerHealth + 1;
-    //        Vector3 temp = blood.transform.position;
-    //        temp.z = 1;
-    //        Vector3 newPosition = new Vector3(blood.transform.position.x, blood.transform.position.y, temp.z);
-    //    }
-    //}
+    public void OnTriggerEnter(Collider other)
+    {
+        setIsOn(true);
+    }
+    public void PlayerInteract()
+    {
+        if (isOn && currentHealth < maxHealth && currentBlood > minBlood && currentBlood <= maxBlood)
+        {
+            bones = bones - 1;
+            currentBlood = currentBlood - 1;
+            currentHealth = currentHealth + 1;
+            Vector3 temp = blood.transform.position;
+            temp.z -= 1;
+            Vector3 newPosition = new Vector3(blood.transform.position.x, blood.transform.position.y, temp.z);
+        }
+    }
 }
