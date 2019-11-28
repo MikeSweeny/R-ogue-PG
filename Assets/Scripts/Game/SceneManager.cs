d3e96bbd880fc09b1953b13eb3d9e9d76f69b3d2
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2019-11-28 14:36:53 -0500

    Player is now referenced in the scene manager
    Started work on the player manager script

--- Assets/Scripts/Game/SceneManager.cs
+++ Assets/Scripts/Game/SceneManager.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    // Declare any public variables that you want to be able 
-    // to access throughout your scene
-    //     public Player player;
-    private bool usingController;
+    // Public references to anything in the scene
+    public PlayerController playerController;
+    public PlayerManager playerManager;
+
     public static SceneManager Instance { get; private set; } // static singleton
     void Awake()
     {
-        if (Instance == null) { Instance = this; }
-        else { Destroy(gameObject); }
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         // Cache references to all desired variables
-        //player = FindObjectOfType<Player>();
+        playerController = FindObjectOfType<PlayerController>();
+        playerManager = FindObjectOfType<PlayerManager>();
     }
 }
