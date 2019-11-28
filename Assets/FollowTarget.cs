1bec097adb8df48e77dcb0022ffd87ebbcfcf30e
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2019-11-28 16:49:22 -0500

    Fixed the Follow

--- Assets/FollowTarget.cs
+++ Assets/FollowTarget.cs
@@ -13,17 +13,15 @@ public class FollowTarget : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-
-
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //target = player.transform.position;
-       // agent.SetDestination(player.position);
+        target = SceneManager.Instance.GetPlayerController().transform.position;
+
+        agent.SetDestination(target.position);
 
     }
 }
