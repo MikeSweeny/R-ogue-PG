38da72ec1bf7c09a10d3f3542e3dfde02adb73d2
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-11-28 14:41:19 -0500

    Blood Alter Script Base
    -Created the beginnings of the blood alter script, commented it out until the proper variables can be checked (player health, bones in inventory)

---
+++ Assets/Scripts/Interactables/BloodAlter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodAlter : Interactable
+{
+    //private int maxBlood = 100;
+    //private int minBlood = 0;
+    //private int currentBlood = 100;
+    //public GameObject blood;
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.E))
+    //    {
+    //        PlayerInteract();
+    //    }
+    //}
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    setIsOn(true);
+    //}
+    //public void PlayerInteract()
+    //{
+    //    if(isOn && playerHealth < maxHealth && currentBlood > minBlood && currentBlood <= maxBlood)
+    //    {
+    //        bones = bones - 1;
+    //        currentBlood = currentBlood - 1;
+    //        playerHealth = playerHealth + 1;
+    //        Vector3 temp = blood.transform.position;
+    //        temp.z = 1;
+    //        Vector3 newPosition = new Vector3(blood.transform.position.x, blood.transform.position.y, temp.z);
+    //    }
+    //}
+}
