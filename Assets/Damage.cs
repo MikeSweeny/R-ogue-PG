65ea4fc9dedabdb87a8074e692725f0752354589
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-11-30 12:44:59 -0500

    didnt push
    these didnt push at the same time by accident

---
+++ Assets/Damage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    PlayerManager pM;
+    int currentHealthDisplay;
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("DamageDealt");
+            Debug.Log(pM.GetHealth());
+            Debug.Log(currentHealthDisplay);
+            damageDealt();
+        }
+    }
+    public void damageDealt()
+    {
+        pM = SceneManager.Instance.GetPlayerManager();
+        pM.SetHealth(-1);
+    }
+}
