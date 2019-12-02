efb5fe804b121b0980a6ae10fe7d308124ebd7d7
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2019-12-01 22:54:00 -0500

    created AI states for the enemies

--- Assets/Scripts/Enemies/AI/AI_Controller/AiController.cs
+++ Assets/Scripts/Enemies/AI/AI_Controller/AiController.cs
@@ -5,6 +5,13 @@ using UnityEngine.AI;
 
 public class AiController : MonoBehaviour
 {
+    public AI_StateAttacking stateAttacking { get; set; }
+    public AI_StateChasing stateChasing { get; set; }
+    public AI_StateDead stateDead { get; set; }
+
+
+    public AIState currentState;
+
     private Animator _animator;
 
     private float x = 0;
@@ -14,70 +21,110 @@ public class AiController : MonoBehaviour
 
     private Collider attackRange;
     private NavMeshAgent agent;
+    private bool isAttacking = false;
+
+    private Rigidbody myRigidbody;
+
+    private Vector3 myVelocity;
+
+    private void Awake()
+    {
+        stateChasing = new AI_StateChasing(this);
+        stateAttacking = new AI_StateAttacking(this);
+        stateDead = new AI_StateDead(this);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        NewAIState(stateChasing);
+
         _animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
 
         attackRange = GetComponentInChildren<SphereCollider>();
-
-        //PlayerCollider = SceneManager.Instance.GetPlayerController();
-
+        
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
-
-        /*if (_animator = null)
+        if (currentState == stateChasing)
         {
-            return;
-        }*/
 
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
-        Vector3 myVelocity = rigidbody.velocity;
 
-        //x = myVelocity.x;
-        //y = myVelocity.y;
-      
-        if (myVelocity.z > 0)
-        {
-            x = x + 0.1f;
-            if (myVelocity.z >= 1)
+            agent.SetDestination(target.position);
+
+            myVelocity = myRigidbody.velocity;
+
+            if (myVelocity.z > 0)
             {
-                x = 1;
+                x = x + 0.1f;
+                if (myVelocity.z >= 1)
+                {
+                    x = 1;
+                }
+
+            }
+            else if (myVelocity.z < 0)
+            {
+                x = x - 0.1f;
+                if (myVelocity.z <= -1)
+                {
+                    x = -1;
+                }
             }
 
-        }
-        else if (myVelocity.z < 0)
-        {
-            x = x - 0.1f;
-            if (myVelocity.z <= -1)
+            if (myVelocity.x > 0)
             {
-                x = -1;
+                y = y + 0.1f;
+                if (myVelocity.x >= 1)
+                {
+                    y = 1;
+                }
             }
+            else if (myVelocity.x < 0)
+            {
+                y = y - 0.1f;
+                if (myVelocity.x <= -1)
+                {
+                    y = -1;
+                }
+            }
+            EnemyMovement(x, y);
+        }
+        if (currentState == stateAttacking)
+        {
+            _animator.SetBool("isAttacking", true);
+            
+            x = 1;
+
+        }
+        else
+        {
+            _animator.SetBool("isAttacking", false);
+
         }
 
-        if (myVelocity.x > 0)
+        if (currentState == stateDead)
         {
-            y = y + 0.1f;
-            if (myVelocity.x >= 1)
-            {
-                y = 1;
-            }
+            x = 0;
+            y = 0;
+            EnemyMovement(x, y);
+
+
+            this.gameObject.SetActive(false);
         }
-        else if (myVelocity.x < 0)
+    }
+    public void NewAIState(AIState newState)
+    {
+        if(currentState != null)
         {
-            y = y - 0.1f;
-            if (myVelocity.x <= -1)
-            {
-                y = -1;
-            }
+            currentState.StateExit();
         }
-        EnemyMovement(x, y);
+        currentState = newState;
+        currentState.StateEnter();
     }
     
     private void EnemyMovement(float x, float y)
@@ -88,7 +135,16 @@ public class AiController : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
-       
+        if (other.gameObject.name == "ModelPlayer_T-Pose")
+        {
+            NewAIState(stateAttacking);
+
+           // _animator.SetBool("isAttacking", true);
+        }
+        /*if (other.gameObject.tag == "Enemy");
+        {
+
+        }*/
     }
 
 
---
+++ Assets/Scripts/Enemies/Enemy_Damage/Enemy_Damage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Damage : MonoBehaviour
+{
+    private Collider myWeapon;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        myWeapon = GetComponent<BoxCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "ModelPlayer_T-Pose")
+        {
+            Debug.Log("Hit");
+        }
+    }
+}
